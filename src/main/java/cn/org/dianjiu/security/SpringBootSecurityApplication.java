59c7cf31b1284da1886bd3c36863ff160e14629c
dianjiu <Limw1124>
2020-06-28 17:35:49 +0800

    初始化基础实现

--- src/main/java/cn/org/dianjiu/security/SpringBootSecurityApplication.java
+++ src/main/java/cn/org/dianjiu/security/SpringBootSecurityApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootSecurityApplication {
 
     public static void main(String[] args) {
-        //System.setProperty("cfg.env","local");
+        System.setProperty("cfg.env","local");
         SpringApplication.run(SpringBootSecurityApplication.class, args);
     }
 
