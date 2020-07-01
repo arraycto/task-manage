fd33a4220e2aa202bc7248b6fc208a15e92dff56
dianjiu <Limw1124>
2020-07-01 14:36:08 +0800

    完善非空判断和字符串转码

--- src/main/java/cn/org/dianjiu/task/common/util/ObjectUtils.java
+++ src/main/java/cn/org/dianjiu/task/common/util/ObjectUtils.java
@@ -62,213 +62,133 @@ public class ObjectUtils {
     }
 
     /**
-     * 判断字符串不为空
+     * 字符串非空判断
      * @param str
      * @return
      */
-    public static boolean notEmpty(String str){
-        //StringUtils.isNotEmpty(str);
-        return str != null && !"".equals(str);
+    public static boolean isEmpty(String str) {
+        return (str == null || str.length() == 0);
     }
 
-    /**
-     * 判断字符串不为空
-     * jdk StringUtils工具类实现如下所示
-     * @param str
-     * @return
-     */
     public static boolean isNotEmpty(String str){
         return !isEmpty(str);
     }
 
-    /**
-     * 判断字符串为空
-     * @param str
-     * @return
-     */
-    public static boolean isEmpty(String str){
-        return str == null || str.length() == 0;
+    public static boolean isBlank(String str) {
+        return ((str == null) || (str.trim().length() == 0));
     }
 
+    public static boolean isNotBlank(String str){
+        return !isBlank(str);
+    }
+
+    /**/
     /**
-     * 集合判断是否为空
-     * @param collection 使用泛型
+     * change UTF8 To GB2312
+     * @param target
      * @return
      */
-    public static <T> boolean notEmpty(Collection<T> collection){
-        if(collection != null){
-            Iterator<T> iterator = collection.iterator();
-            if(iterator != null){
-                while(iterator.hasNext()){
-                    Object next = iterator.next();
-                    if(next != null){
-                        return true;
-                    }
-                }
-            }
+    public static final String UTF82GB2312(String target) {
+        try {
+            return new String(target.getBytes("UTF-8"), "gb2312");
+        } catch (Exception localException) {
+            System.err.println("UTF8 TO GB2312 change error!");
         }
-        return false;
+        return null;
     }
 
     /**
-     * map集合不为空的判断
-     * @param map 使用泛型，可以传递不同的类型参数
+     * change UTF8 To GBK
+     * @param target
      * @return
      */
-    public static <T> boolean notEmpty(Map<T, T> map){
-        return map != null && !map.isEmpty();
+    public static final String UTF82GBK(String target) {
+        try {
+            return new String(target.getBytes("UTF-8"), "GBK");
+        } catch (Exception localException) {
+            System.err.println("UTF8 TO GBK change error!");
+        }
+        return null;
     }
 
     /**
-     * byte类型数组判断不为空
-     * @param t
+     * change UTF8 To ISO8859-1
+     * @param target
      * @return
      */
-    public static boolean notEmpty(byte[] t){
-        return t != null && t.length > 0;
+    public static final String UTF82ISO(String target) {
+        try {
+            return new String(target.getBytes("UTF-8"), "ISO8859-1");
+        } catch (Exception localException) {
+            System.err.println("UTF8 TO ISO8859-1 change error!");
+        }
+        return null;
     }
 
     /**
-     * short类型数组不为空判断
-     * @param t
+     * change Windows-1252 To UTF-8
+     * @param target
      * @return
      */
-    public static boolean notEmpty(short[] t){
-        return t != null && t.length > 0;
+    public static final String Windows1252UTF8(String target) {
+        try {
+            return new String(target.getBytes("Windows-1252"), "UTF-8");
+        } catch (Exception localException) {
+            System.err.println("Windows1252 To UTF8 chage error");
+        }
+        return null;
     }
 
-    /**
-     * 数组判断不为空,没有泛型数组,所以还是分开写吧
-     * @param t 可以是int,short,byte,String,Object,long
-     * @return
-     */
-    public static boolean notEmpty(int[] t){
-        return t != null && t.length > 0;
+
+    public static boolean isEmpty(Long l) {
+        return ((l == null) || (l.longValue() == 0L));
     }
 
-    /**
-     * long类型数组不为空
-     * @param t
-     * @return
-     */
-    public static boolean notEmpty(long[] t){
-        return t != null && t.length > 0;
+    public static boolean isNotEmpty(Long l){
+        return !isEmpty(l);
     }
 
-    /**
-     * String类型的数组不为空
-     * @param t
-     * @return
-     */
-    public static boolean notEmpty(String[] t){
-        return t != null && t.length > 0;
+    public static boolean isEmpty(Object objs) {
+        return (objs == null);
     }
 
-    /**
-     * Object类型数组不为空
-     * @param t
-     * @return
-     */
-    public static boolean notEmpty(Object[] t){
-        return t != null && t.length > 0;
+    public static boolean isNotEmpty(Object objs){
+        return !isEmpty(objs);
     }
 
-    /**
-     *
-     * @param o
-     * @return
-     */
-    public static boolean notEmpty(Object o){
-        return o != null && !"".equals(o) && !"null".equals(o);
+    public static boolean isEmpty(Object[] objs) {
+        return ((objs == null) || (objs.length == 0));
+    }
+
+    public static boolean isNotEmpty(Object[] objs){
+        return !isEmpty(objs);
+    }
+
+    public static boolean isEmpty(Collection<?> objs) {
+        return ((objs == null) || (objs.size() <= 0));
+    }
+
+    public static boolean isNotEmpty(Collection<?> objs){
+        return !isEmpty(objs);
+    }
+
+    public static boolean isEmpty(byte[] objs) {
+        return ((objs == null) || (objs.length == 0));
+    }
+
+    public static boolean isNotEmpty(byte[] objs){
+        return !isEmpty(objs);
     }
 
     public static void main(String[] args) {
-        //String str = "";
-        //1、判断字符串是否为空notEmpty()方法
-        /*if(ObjectUtils.notEmpty(str)){
-            System.out.println("字符串 " + str + " 不为空......");
-        }else{
-            System.out.println("字符串 " + str + "为空......");
-        }*/
-
-        //2、判断字符串是否为空isNotEmpty()方法
-        /*if(ObjectUtils.isNotEmpty(str)){
-            System.out.println("字符串 " + str + " 不为空......");
-        }else{
-            System.out.println("字符串 " + str + "为空......");
-        }*/
-
-        //3、集合判断是否为空,list和set实现Collection
-        /*List<String> list = new ArrayList<String>();
-        //list.add("hello");
-        if(ObjectUtils.notEmpty(list)){
-            System.out.println("List集合不为空");
-        }else{
-            System.out.println("List集合为空");
-        }*/
-
-        /*Set<String> set = new HashSet<String>();
-        set.add("hello");
-        if(ObjectUtils.notEmpty(set)){
-            System.out.println("set集合不为空");
-        }else{
-            System.out.println("set集合为空");
-        }*/
-
-        //4、map集合接口,需要写单独的判读是否为空的方法
-        /*Map<String, String> map = new HashMap<String, String>();
-        //map.put("hello", "hello world");
-        if(ObjectUtils.notEmpty(map)){
-            System.out.println("map集合不为空");
-        }else{
-            System.out.println("map集合为空");
-        }*/
-
-        //5、数组判断不为空
-        /*int[] a = new int[]{1,2,3,4,5};
-        if(ObjectUtils.notEmpty(a)){
-            System.out.println("int类型数组不为空");
-        }else{
-            System.out.println("int类型数组为空");
-        }*/
-
-        /*byte[] b = new byte[]{1,2,3,4,5};
-        if(ObjectUtils.notEmpty(b)){
-            System.out.println("byte类型数组不为空");
-        }else{
-            System.out.println("byte类型数组为空");
-        }
-
-        short[] c = new short[]{1,2,3,4,5};
-        if(ObjectUtils.notEmpty(c)){
-            System.out.println("short类型数组不为空");
-        }else{
-            System.out.println("short类型数组为空");
-        }
-
-
-        long[] d = new long[]{1,2,3,4,5};
-        if(ObjectUtils.notEmpty(d)){
-            System.out.println("long类型数组不为空");
-        }else{
-            System.out.println("long类型数组为空");
-        }
-
-
-        String[] e = new String[]{"hello","world","lisi","zhangsan"};
-        if(ObjectUtils.notEmpty(e)){
-            System.out.println("String类型数组不为空");
-        }else{
-            System.out.println("String类型数组为空");
-        }
-
-        Object[] a = new Object[]{1,2,3,4,5};
-        if(ObjectUtils.notEmpty(a)){
-            System.out.println("Object类型数组不为空");
-        }else{
-            System.out.println("Object类型数组为空");
-        }*/
-
-
+        System.out.println(ObjectUtils.isEmpty((String) null));        //true
+        System.out.println(ObjectUtils.isEmpty(""));          //true
+        System.out.println(ObjectUtils.isEmpty("   "));       //false
+        System.out.println(ObjectUtils.isEmpty("dd"));        //false
+        System.out.println("----------------------------");
+        System.out.println(ObjectUtils.isBlank(null));        //true
+        System.out.println(ObjectUtils.isBlank(""));          //true
+        System.out.println(ObjectUtils.isBlank("   "));       //true
+        System.out.println(ObjectUtils.isBlank("dd"));        //false
     }
 }
